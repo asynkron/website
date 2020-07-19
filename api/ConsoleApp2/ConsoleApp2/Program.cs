98789585f88eb5808fbbd80ec55b7ca5e2cca3a7
rogeralsing <rogeralsing@gmail.com>
2020-07-19 19:32:26 +0200

    .

---
+++ api/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var x = new Some();
+
+            await foreach (var y in AsyncStuff())
+            {
+                
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+
+        static async List<Task<string>> AsyncStuff()
+        {
+            
+        }
+    }
+
+    public class Some : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
