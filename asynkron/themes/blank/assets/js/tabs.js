a7c0d82cafc3f242479be0530a05beb3911361d3
rogeralsing <rogeralsing@gmail.com>
2020-08-03 21:39:11 +0200

    .

--- asynkron/themes/blank/assets/js/tabs.js
+++ asynkron/themes/blank/assets/js/tabs.js
@@ -4,17 +4,14 @@ $(document).ready(function () {
     $('.tab-content').find('.tab-pane').each(function (idx, item) {
 
         var navTabs = $(this).closest('.code-tabs').find('.nav-tabs');
-        title = $(this).attr('title') + tabId;
+        title = $(this).attr('title');
         $(this).attr("id","tab" + tabId);
-        if (idx == 0) {
+        if (idx == 0) { //first tab. activate it
             $(this).addClass("active");
             navTabs.append('<li class="nav-item"><a class="nav-link active" data-toggle="tab" role="tab" aria-selected="true" href="#tab'+tabId+'" aria-controls="tab' + tabId + '" >' + title + '</a></li');
         } else {
             navTabs.append('<li class="nav-item"><a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#tab'+tabId+'" aria-controls="tab' + tabId + '" >' + title + '</a></li');
         }
-        
-        
-        
 
         tabId++;
     });
